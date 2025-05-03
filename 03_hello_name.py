af24259162d012b613c981335e50ddbd71547777
Diego Gerwig <diegogerwig@gmail.com>
2025-05-04 00:17:18 +0200

    update

---
+++ 03_hello_name.py
@@ -0,0 +1,13 @@
+def main():
+    """
+    This program asks for the user's name and then greets them.
+    """
+    # Prompt the user for their name
+    name = input("What is your name? ")
+
+    # Print the greeting
+    print("Hello", name)
+
+
+if __name__ == '__main__':
+    main()
