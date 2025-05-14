090ae201ea47cd2daa954c61489e929936b8f27a
Diego Gerwig <diegogerwig@gmail.com>
2025-05-14 21:41:23 +0200

    update

---
+++ 05_draw_flag.py
@@ -0,0 +1,17 @@
+from graphics import Canvas
+
+CANVAS_WIDTH = 450
+CANVAS_HEIGHT = 300
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    
+    # Draw the red top half of the flag
+    canvas.create_rectangle(
+        0, 0,                # Top-left corner
+        CANVAS_WIDTH, CANVAS_HEIGHT / 2,  # Bottom-right corner
+        "red"               # Fill color
+    )
+
+if __name__ == '__main__':
+    main()
